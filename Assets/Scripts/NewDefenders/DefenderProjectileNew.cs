36e97eab689a284321930fc09cbe3129b210e009
SpartanKick <ST10208767@vgconnect.edu.za>
2024-11-11 11:40:14 +0200

    vfx working

--- Assets/Scripts/NewDefenders/DefenderProjectileNew.cs
+++ Assets/Scripts/NewDefenders/DefenderProjectileNew.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public abstract class DefenderProjectileNew : MonoBehaviour
 {
     protected float speed;
-    protected int damage; 
+    protected int damage;
     protected GameObject target;
 
-    [SerializeField] private BulletTrailScriptableObject bulletTrailConfig;
-    [SerializeField] private GameObject damageParticleEffectPrefab; // Use GameObject type for the prefab
+     public BulletTrailScriptableObject bulletTrailConfig;
+     public GameObject damageParticleEffectPrefab; // Use GameObject type for the prefab
     private TrailRenderer trailRenderer;
 
     public void Initialize(GameObject target, float projectileSpeed, int projectileDamage)
@@ -31,7 +31,7 @@ public abstract class DefenderProjectileNew : MonoBehaviour
         {
             Vector3 direction = (target.transform.position - transform.position).normalized;
             transform.position += direction * speed * Time.deltaTime;
-            
+
             if (Vector3.Distance(transform.position, target.transform.position) < 0.5f)
             {
                 DealDamage();
@@ -40,15 +40,30 @@ public abstract class DefenderProjectileNew : MonoBehaviour
 
             yield return null;
         }
-        Destroy(gameObject); 
+        Destroy(gameObject);
     }
 
     protected virtual void DealDamage()
     {
+        Debug.Log("IN DEAL DAMAGE");
         // Instantiate and play the particle effect at the point of impact
         if (damageParticleEffectPrefab != null)
         {
-            Instantiate(damageParticleEffectPrefab, transform.position, Quaternion.identity).GetComponent<ParticleSystem>().Play();
+            GameObject particleEffect = Instantiate(damageParticleEffectPrefab, transform.position, Quaternion.identity);
+            ParticleSystem ps = particleEffect.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                Debug.Log("Particle effect instantiated and found.");
+                ps.Play();
+            }
+            else
+            {
+                Debug.LogError("Particle system component not found on the instantiated prefab!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Damage particle effect prefab is not assigned!");
         }
     }
 }
--- Assets/Scripts/NewDefenders/RaptorProjectileNew.cs
+++ Assets/Scripts/NewDefenders/RaptorProjectileNew.cs
@@ -10,11 +10,23 @@ public class RaptorProjectileNew : DefenderProjectileNew
 
     protected override void DealDamage()
     {
-        
         EnemyNew enemy = target.GetComponent<EnemyNew>();
         if (enemy != null)
         {
             enemy.TakeDamage(damage);
+            
+            // Instantiate and play the particle effect
+            GameObject particleEffect = Instantiate(damageParticleEffectPrefab, transform.position, Quaternion.identity);
+            ParticleSystem ps = particleEffect.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                ps.Play();
+                Destroy(particleEffect, ps.main.duration); // Destroy particle effect after its duration
+            }
+            else
+            {
+                Debug.LogError("Particle system component not found on the instantiated prefab!");
+            }
         }
         else
         {
